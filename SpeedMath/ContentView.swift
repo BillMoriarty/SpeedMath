0d379e78c968dff6aafc264cf3ec907b1bbea09f
billmoriarty <wrmoriarty@gmail.com>
2020-03-20 10:11:37 -0400

    Initial Commit

---
+++ SpeedMath/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SpeedMath
+//
+//  Created by Bill Moriarty on 3/20/20.
+//  Copyright © 2020 Bill Moriarty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
